bc7e31b7b49d2332fcbc5ca3cbbebab3042f1690
augustosimoes <yep4i8sx>
2017-09-12 20:28:10 -0300

    Adicionando arquivos da pasta Src

---
+++ Src/testes.py
@@ -0,0 +1,19 @@
+Import jogovelha
+import sys
+
+erroInicializar = False
+jogo = jogovelha.inicializar()
+
+if len(jogo) != 3:
+	erroInicializar = True
+else:
+	for linha in jogo:
+		if len(linha) != 3:
+			erroInicializar = True
+		else:
+			for elemento in linha:
+				erro Inicializar = True
+if erroInicializar:
+	sys.exit(1)
+else:
+	sys.exit(0)
